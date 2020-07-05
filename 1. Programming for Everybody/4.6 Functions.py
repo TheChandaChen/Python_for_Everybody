732a748bd43e9f86f601d584c34e9c6897aab193
Chanda Chen <TheChandaChen@gmail.com>
2020-07-04 22:13:31 -0400

    Clean code and add notes

--- 1. Programming for Everybody/4.6 Functions.py
+++ 1. Programming for Everybody/4.6 Functions.py
@@ -1,18 +1,22 @@
-def computepay(h,r):
-    if h < 40:
-        pay = h * r
-        print (p)
+# computepay is computing the user's pay
+def computepay(h, r):
+    if h <= 40:
+        p = h * r
     else:
-        reg = 40 * r
-        h = h - 40
-        r = r * 1.5
-        pay = reg + (h * r)
-    return pay
+        overtime = (h - 40) * (1.5 * r)
+        p = (40 * r) + overtime
+    return p
 
-hrs = input("Enter Hours: ")
-rate = input("Enter Rate: ")
-h = float(hrs)
-r = float(rate)
 
-p = computepay(h,r)
-print(p)
+hrs = input('Enter Hours: ')
+rate = input('Enter Rate: ')
+# Use try/except to catch any problems a user may input
+try:
+    h = float(hrs)
+    r = float(rate)
+except:
+    print("Error, enter numeric input.")
+    quit()   # Ends the program. Run program again to correct the inputs
+
+p = computepay(h, r)
+print("Pay", p)
