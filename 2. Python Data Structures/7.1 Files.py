a2b8817df9197d4ac36fc23df1ee37590506cd45
Chanda Chen <TheChandaChen@gmail.com>
2020-07-14 21:24:30 -0400

    Clean code and add notes

---
+++ 2. Python Data Structures/7.1 Files.py
@@ -0,0 +1,14 @@
+fname = input("Enter file name: ")
+
+#ensures file name is typed in properly and exists
+try:
+    fh = open(fname)
+except:
+    print("File cannot be opened")
+    quit()
+
+#removes whitespace/newlines and capitalizes all characters
+for lines in fh:
+    lines = lines.rstrip()
+    lines = lines.upper()
+    print(lines)
--- 2. Python Data Structures/Assignment_7.1.py
+++
@@ -1,7 +0,0 @@
-# Use words.txt as the file name
-fname = input("Enter file name: ")
-fh = open(fname)
-for line in fh:
-    cap = line.upper()
-    cap = cap.rstrip()
-    print(cap)
