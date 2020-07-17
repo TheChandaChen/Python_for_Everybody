8aa8958e1eb1f73643e3cd665f36f6deffed6b5c
Chanda Chen <TheChandaChen@gmail.com>
2020-07-17 10:54:20 -0400

    Complete assignment

---
+++ 2. Python Data Structures/10.2 Tuples.py
@@ -0,0 +1,18 @@
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+
+# creates a dictionary of hours and a count of the hours by doing multiple splits
+count = dict()
+for email in handle:
+    if email.startswith("From "):
+        words = email.split()
+        time = words[5]
+        split_time = time.split(':')
+        hour = split_time[0]
+        count[hour] = count.get(hour, 0) + 1
+
+#creates a list with the key/values from dictionary and sorts the list by hour(key)
+hour_counter = list()
+for hour, counter in sorted(count.items()):
+    print(hour, counter)
