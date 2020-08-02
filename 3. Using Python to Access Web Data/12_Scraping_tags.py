9f5f7d664655a04fcf3bc92c175d5063d6928452
Chanda Chen <TheChandaChen@gmail.com>
2020-08-01 21:46:36 -0400

    Complete assignment

---
+++ 3. Using Python to Access Web Data/12_Scraping_tags.py
@@ -0,0 +1,30 @@
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+import re
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+sum = 0
+
+# Retrieve all span tags
+tags = soup('span')
+
+# Split span tags to only have numbers
+for tag in tags:
+    string = str(tag)
+    comments_split = string.split('>')
+    comments_unfinished = comments_split[1]
+    comments_split_twice = comments_unfinished.split('<')
+    comments = comments_split_twice[0]
+    comments = int(comments)
+    sum = sum + comments
+
+print(sum)
